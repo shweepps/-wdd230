e432c82cad0331dce84ee2b8b1cca418d8cc8868
shweepps <shweepps@gmail.com>
2023-05-14 11:23:10 +0200

    chamber

---
+++ chamber/scripts/script.js
@@ -0,0 +1,30 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    const Fullyear = new Date().getFullYear();
+    const msgFooter = `&copy; ${Fullyear} | `;
+    const msgSecond = `Last Updated: ${document.lastModified}`;
+    const contInfo = ``;
+    const map = `<p>Map <a href="https://www.google.com/maps/place/9+Van+Riebeeck+St,+Fauresmith,+9978/@-29.7489724,25.3165676,17z/data=!3m1!4b1!4m5!3m4!1s0x1e8574e4f9d4e64d:0xa93a42a7002f5d56!8m2!3d-29.7489724!4d25.3165676"> Link </a></p>`;
+    const logo = `
+                    <img width="50" src="images/SACCI-logo.png">`;
+    const siteName = `SACCI | Moshoeshoe S Mopeli | `;
+    const datefield = document.querySelector("time");
+    const now = new Date();
+    const fulldate = new Intl.DateTimeFormat("en-US", 
+        { dateStyle: "full" }).format(
+            now
+        );
+    
+
+    datefield.textContent = fulldate;
+    
+    
+    const footeEl = document.querySelector('footer');
+
+
+    footeEl.innerHTML = msgFooter +siteName + logo + msgSecond + map;
+
+
+
+});
+
