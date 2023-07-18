c0218def5261cd3b2328a9e4fd77e38b3231f10e
shweepps <shweepps@gmail.com>
2023-07-18 23:56:31 +0200

    frsh

--- final-project/scripts/script.js
+++ final-project/scripts/script.js
@@ -98,10 +98,137 @@ async function apiDay() {
     }
 }
 
-    
-    
-
     api();
     apiDay();
+
+// Global variable to store the nutritional data fetched from 'fruit.json'
+let nutritionalData;
+
+// Function to fetch JSON data from 'fruit.json' file
+async function fetchFruitData() {
+    try {
+        const response = await fetch('fruit.json');
+        if (response.ok) {
+            nutritionalData = await response.json();
+            return nutritionalData;
+        } else {
+            throw new Error('Failed to fetch fruit data');
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Function to populate the select element with fruit options
+async function populateSelectElement(selectElement) {
+    await fetchFruitData();
+    if (nutritionalData) {
+        nutritionalData.forEach(fruit => {
+            const option = document.createElement("option");
+            option.value = fruit.name;
+            option.textContent = fruit.name;
+            selectElement.appendChild(option);
+        });
+    }
+}
+
+// Get the form element
+const form = document.getElementById("specialtyDrinkForm");
+
+// Get the select elements
+const fruitSelect1 = document.getElementById("fruit1");
+const fruitSelect2 = document.getElementById("fruit2");
+const fruitSelect3 = document.getElementById("fruit3");
+
+// Populate the select elements with fruit options
+populateSelectElement(fruitSelect1);
+populateSelectElement(fruitSelect2);
+populateSelectElement(fruitSelect3);
+
+// Function to calculate the total nutritional values based on selected fruits
+function calculateTotalNutrition(fruits) {
+    let totalCarbohydrates = 0;
+    let totalProtein = 0;
+    let totalFat = 0;
+    let totalSugar = 0;
+    let totalCalories = 0;
+
+    fruits.forEach(fruitName => {
+        const fruit = nutritionalData.find(item => item.name === fruitName);
+        if (fruit) {
+            totalCarbohydrates += fruit.nutritions.carbohydrates;
+            totalProtein += fruit.nutritions.protein;
+            totalFat += fruit.nutritions.fat;
+            totalSugar += fruit.nutritions.sugar;
+            totalCalories += fruit.nutritions.calories;
+        }
+    });
+
+    return {
+        totalCarbohydrates,
+        totalProtein,
+        totalFat,
+        totalSugar,
+        totalCalories
+    };
+}
+
+// Function to handle form submission
+function handleSubmit(event) {
+    event.preventDefault();
+
+    // Extract form values
+    const firstName = document.getElementById("firstName").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const selectedFruit1 = fruitSelect1.value;
+    const selectedFruit2 = fruitSelect2.value;
+    const selectedFruit3 = fruitSelect3.value;
+    const specialInstructions = document.getElementById("specialInstructions").value;
+
+    // Create an object to store the form data
+    const formData = {
+        firstName: firstName,
+        email: email,
+        phoneNumber: phoneNumber,
+        selectedFruit1: selectedFruit1,
+        selectedFruit2: selectedFruit2,
+        selectedFruit3: selectedFruit3,
+        specialInstructions: specialInstructions
+    };
+
+    // Calculate total nutritional values
+    const selectedFruits = [selectedFruit1, selectedFruit2, selectedFruit3];
+    const totalNutrition = calculateTotalNutrition(selectedFruits);
+
+    // Store the form data in local storage
+    localStorage.setItem('formData', JSON.stringify(formData));
+
+    // Clear the form after submission
+    form.reset();
+
+    // Display the form data and nutritional values
+    const displaySection = document.getElementById("displaySection");
+    displaySection.innerHTML = `
+        <h2>Submitted Form Data:</h2>
+        <p><strong>First Name:</strong> ${formData.firstName}</p>
+        <p class="E"><strong>Email:</strong> ${formData.email}</p>
+        <p><strong>Phone Number:</strong> ${formData.phoneNumber}</p>
+        <p><strong>Fruit 1:</strong> ${formData.selectedFruit1}</p>
+        <p><strong>Fruit 2:</strong> ${formData.selectedFruit2}</p>
+        <p><strong>Fruit 3:</strong> ${formData.selectedFruit3}</p>
+        <p><strong>Special Instructions:</strong> ${formData.specialInstructions}</p>
+        <h2>Total Nutritional Values:</h2>
+        <p><strong>Total Carbohydrates:</strong> ${totalNutrition.totalCarbohydrates.toFixed(1)} g</p>
+        <p><strong>Total Protein:</strong> ${totalNutrition.totalProtein.toFixed(1)} g</p>
+        <p><strong>Total Fat:</strong> ${totalNutrition.totalFat.toFixed(1)} g</p>
+        <p><strong>Total Sugar:</strong> ${totalNutrition.totalSugar.toFixed(1)} g</p>
+        <p><strong>Total Calories:</strong> ${totalNutrition.totalCalories.toFixed(1)} kcal</p>
+    `;
+}
+
+// Add event listener for form submission
+form.addEventListener("submit", handleSubmit);
+
 
 });
